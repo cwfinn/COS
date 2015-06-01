ecafe20d5ff36eed7c6b5d4d646f611db8e93033
Charles Finn <c.w.finn2301@gmail.com>
2015-06-01 16:10:44 +0100

    Init repository

---
+++ setup.py
@@ -0,0 +1,42 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+from Cython.Build import cythonize
+
+import numpy
+
+setup(name='COS',
+      version=0.1,
+      packages=find_packages(),
+      description='Utilities for data taken with the Cosmic Origins '
+                  'Spectrograph on the Hubble Space Telescope',
+      setup_requires=['cython'],
+      install_requires=['astropy>=1.0', 'scipy', 'matplotlib'],
+      author='Charles Finn',
+      author_email='c.w.finn2301@gmail.com',
+      license='BSD',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Cython',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Physics'
+      ],
+      provides=['COS'],
+      zip_safe=False,
+      use_2to3=False,
+      entry_points={
+          'console_scripts': [
+              'runcalcos = COS.reduction:main'
+          ]
+      },
+      ext_modules=cythonize('COS/_convolve.pyx'),
+      include_dirs=[numpy.get_include(), ],
+      include_package_data=True
+      )
